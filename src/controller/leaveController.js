a51fd0d803b79df0f598c3ac423bd0ab1a811491
Sk Mursalin <skmursalin02@gmail.com>
2025-12-04 12:32:38 +0530

    leave manage ment

--- src/controller/leaveController.js
+++ src/controller/leaveController.js
@@ -32,4 +32,63 @@ const getLeaves = async (req, res) => {
     }
 };
 
-export { addLeave , getLeaves }
+const getViewLeaves = async (req, res) => {
+    try {
+        const leaves = await Leave.find().populate({
+            path: "employeeId",
+            populate: [
+                {
+                    path: 'department',
+                    select: 'dep_name'
+                },
+                {
+                    path: 'userID',
+                    select: 'name'
+                }
+            ]
+        })
+        return res.status(200).json({ success: true, leaves })
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ success: false, error: "leave add server error" })
+    }
+}
+
+const leaveDetails = async (req, res) => {
+    try {
+        const { id } = req.params
+        const leave = await Leave.findById(id).populate({
+            path: "employeeId",
+            populate: [
+                {
+                    path: 'department',
+                    select: 'dep_name'
+                },
+                {
+                    path: 'userID',
+                    select: 'name'
+                }
+            ]
+        })
+        return res.status(200).json({ success: true, leave })
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ success: false, error: "leave send server error" })
+    }
+}
+
+const updateLeaveStatus = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const leave = await Leave.findByIdAndUpdate({ _id: id }, { status: req.body.status });
+        if(!leave) {
+            return res.status(404).json({success: false, error: "leave not founded"})
+        }
+        return res.status(200).json({success: true})
+    } catch(error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "leave update server error"})
+    }
+}
+
+export { addLeave, getLeaves, getViewLeaves , leaveDetails , updateLeaveStatus }
--- src/routes/leave.js
+++ src/routes/leave.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import authMiddleware from '../middlewares/authmiddleware.js'
-import { addLeave, getLeaves } from '../controller/leaveController.js'
+import { addLeave, getLeaves, getViewLeaves, leaveDetails, updateLeaveStatus } from '../controller/leaveController.js'
 
 const leaveRouter = express.Router()
 
 leaveRouter.post('/add', authMiddleware, addLeave)
 leaveRouter.get('/:id', authMiddleware, getLeaves)
+leaveRouter.put('/:id', authMiddleware, updateLeaveStatus)
+leaveRouter.get('/details/:id', authMiddleware, leaveDetails)
+leaveRouter.get('/', authMiddleware, getViewLeaves)
+
 
 
 export default leaveRouter
